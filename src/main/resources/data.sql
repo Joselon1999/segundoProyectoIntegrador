004db33da844bd6260e2874683078f524904f8ef
Jose <joselon1999@hotmail.com>
2024-12-08 15:21:33 -0500

    feat: añadiendo usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,7 @@ VALUES
 INSERT INTO USERS (username, email, password, enabled)
 VALUES
     ('u19300224@utp.edu.pe', 'u19300224@utp.edu.pe', '$2a$12$cvT5dLzgwyPRUKywdYCtXeg2J7SNSyJGAsrhDJjJnuQNiZz.K/fxu', true),
+    ('U22232669@utp.edu.pe', 'U22232669@utp.edu.pe', '$2a$12$cvT5dLzgwyPRUKywdYCtXeg2J7SNSyJGAsrhDJjJnuQNiZz.K/fxu', true),
     ('helper1@mail.com', 'helper1@mail.com', '$2a$12$cvT5dLzgwyPRUKywdYCtXeg2J7SNSyJGAsrhDJjJnuQNiZz.K/fxu', true);
 
 INSERT INTO USER_ROLES (id_usuario, id_rol)
